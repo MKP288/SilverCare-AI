b13ba3e1d80f39dd02efe8eeb8bac65c821f95fc
Yuewei Zhou <yueweizhou@YueweideMacBook-Air.local>
2026-03-21 14:07:28 -0400

    Added backend

---
+++ backend/database/models.py
@@ -0,0 +1,9 @@
+from .connection import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    email = Column(String(100))
